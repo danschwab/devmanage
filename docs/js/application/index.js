0b9a58a1fb0abca678cbb0872e25d2a73f157547
Daniel Schwab <dschwabdesign@gmail.com>
2025-07-15 10:44:44 -0400

    index order matters

--- docs/js/application/index.js
+++ docs/js/application/index.js
@@ -1,8 +1,10 @@
-// Content components export
-export { PacklistContent } from './components/content/PacklistContent.js';
-export { InventoryContent } from './components/content/InventoryContent.js';
-export { InterfacesContent } from './components/content/InterfacesContent.js';
-export { TestTableComponent } from './components/content/testTableComponent.js';
+// Utils
+export { html } from './utils/template-helpers.js';
+export { Auth, authState, useAuth } from './utils/auth.js';
+
+// Navigation components
+export { BreadcrumbComponent } from './components/navigation/breadcrumbComponent.js';
+export { PrimaryNavComponent, NavigationConfig } from './components/navigation/navigationManagement.js';
 
 // Dashboard components export
 export { DashboardManagement, DashboardToggleComponent, DashboardManagementComponent, DashboardSettings } from './components/dashboard/DashboardManagement.js';
@@ -13,13 +15,11 @@ export { ContainerComponent, containerManager } from './components/interface/con
 export { ModalComponent, modalManager } from './components/interface/modalComponent.js';
 export { TableComponent } from './components/interface/tableComponent.js';
 
-// Navigation components
-export { BreadcrumbComponent } from './components/navigation/breadcrumbComponent.js';
-export { PrimaryNavComponent, NavigationConfig } from './components/navigation/navigationManagement.js';
-
-// Utils
-export { Auth, authState, useAuth } from './utils/auth.js';
-export { html } from './utils/template-helpers.js';
+// Content components export
+export { TestTableComponent } from './components/content/testTableComponent.js';
+export { PacklistContent } from './components/content/PacklistContent.js';
+export { InventoryContent } from './components/content/InventoryContent.js';
+export { InterfacesContent } from './components/content/InterfacesContent.js';
 
 // API
 export { Requests } from '../data_management/api.js';
