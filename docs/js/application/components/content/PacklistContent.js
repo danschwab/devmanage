02b15eadd6585bf76c09b66c74c9f4e67ad68dbd
Daniel Schwab <dschwabdesign@gmail.com>
2025-07-16 16:01:50 -0400

    tab management component

--- docs/js/application/components/content/PacklistContent.js
+++ docs/js/application/components/content/PacklistContent.js
@@ -1,4 +1,5 @@
 import { html, modalManager, hamburgerMenuRegistry } from '../../index.js';
+import { TabComponent } from '../interface/tabComponent.js';
 
 export const PacklistMenuComponent = {
     props: {
@@ -42,8 +43,24 @@ export const PacklistMenuComponent = {
 
 
 export const PacklistContent = {
+    components: {
+        TabComponent
+    },
     props: {
         showAlert: Function
+    },
+    data() {
+        return {
+            tabs: [
+                {
+                    name: 'main-packlist',
+                    label: 'Main Packlist',
+                    closable: false,
+                    content: '<div><p>Main packlist content goes here.</p></div>'
+                }
+            ],
+            activeTab: 'main-packlist'
+        };
     },
     computed: {
         // Expose NavigationConfig to the template
@@ -64,10 +81,55 @@ export const PacklistContent = {
             }
         });
     },
+    methods: {
+        handleTabChange(tabName) {
+            this.activeTab = tabName;
+        },
+        handleTabClose(tabName) {
+            const idx = this.tabs.findIndex(t => t.name === tabName);
+            if (idx !== -1) {
+                this.tabs.splice(idx, 1);
+                // Open previous tab if possible, otherwise next tab, otherwise none
+                if (this.activeTab === tabName) {
+                    if (this.tabs.length === 0) {
+                        this.activeTab = '';
+                    } else if (idx > 0) {
+                        this.activeTab = this.tabs[idx - 1].name;
+                    } else {
+                        this.activeTab = this.tabs[0].name;
+                    }
+                }
+            }
+        },
+        handleNewTab() {
+            // Ensure unique tab name
+            let newTabIdx = this.tabs.length + 1;
+            let newTabName = `packlist-${newTabIdx}`;
+            while (this.tabs.some(tab => tab.name === newTabName)) {
+                newTabIdx++;
+                newTabName = `packlist-${newTabIdx}`;
+            }
+            this.tabs.push({
+                name: newTabName,
+                label: `Packlist ${newTabIdx}`,
+                closable: true,
+                content: `<div><p>New packlist tab #${newTabIdx}.</p></div>`
+            });
+            this.activeTab = newTabName;
+        }
+    },
     template: html `
         <div class="packlist-page">
             <h3>Pack List Management</h3>
             <p>Create and manage pack lists for exhibits and events.</p>
+            <TabComponent
+                :tabs="tabs"
+                :active-tab="activeTab"
+                :show-new-tab-button="true"
+                @tab-change="handleTabChange"
+                @tab-close="handleTabClose"
+                @new-tab="handleNewTab"
+            />
             <div style="margin-top: 1rem;">
                 <button @click="modalManager.showAlert('Create new pack list functionality coming soon!', 'Info')">Create New Pack List</button>
                 <button @click="modalManager.showAlert('Import from Inventor functionality coming soon!', 'Info')">Import from Inventor</button>
---
+++ docs/js/application/components/interface/tabComponent.js
@@ -0,0 +1,137 @@
+import { html } from '../../index.js';
+
+// TabComponent: Modular tab navigation for application use
+export const TabComponent = {
+    props: {
+        tabs: {
+            type: Array,
+            required: true // [{ name, label, closable, content }]
+        },
+        activeTab: {
+            type: String,
+            default: ''
+        },
+        showNewTabButton: {
+            type: Boolean,
+            default: false
+        }
+    },
+    emits: ['tab-change', 'tab-close', 'new-tab', 'hamburger-menu'],
+    data() {
+        return {
+            internalActiveTab: this.activeTab || (this.tabs[0]?.name || ''),
+            isOverflowing: false
+        };
+    },
+    watch: {
+        activeTab(val) {
+            this.internalActiveTab = val;
+        },
+        tabs: {
+            handler() {
+                this.$nextTick(() => this.checkOverflow());
+            },
+            deep: true
+        },
+        internalActiveTab() {
+            // Watch for tab open/close and check overflow
+            this.$nextTick(() => this.checkOverflow());
+        }
+    },
+    mounted() {
+        window.addEventListener('resize', this.checkOverflow);
+        this.$nextTick(() => this.checkOverflow());
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.checkOverflow);
+    },
+    methods: {
+        selectTab(tabName) {
+            this.internalActiveTab = tabName;
+            this.$emit('tab-change', tabName);
+        },
+        closeTab(tabName) {
+            this.$emit('tab-close', tabName);
+        },
+        addNewTab() {
+            this.$emit('new-tab');
+            // After adding a tab, verify dropdown mode
+            this.$nextTick(() => this.checkOverflow());
+        },
+        openHamburgerMenu() {
+            const tabsEl = this.$el?.querySelector('.tabs');
+            if (!tabsEl) return;
+            tabsEl.classList.toggle('menu-open');
+            this.$emit('hamburger-menu');
+        },
+        checkOverflow(openMenu = false) {
+            const tabsEl = this.$el?.querySelector('.tabs');
+            if (!tabsEl) return;
+            // Remove dropdown-mode and menu-open to allow layout recalculation
+            tabsEl.classList.remove('dropdown-mode');
+            tabsEl.classList.remove('menu-open');
+            // Force reflow for accurate measurement
+            void tabsEl.offsetWidth;
+            // Use scrollWidth vs clientWidth to detect overflow
+            const isOverflowing = tabsEl.scrollWidth > tabsEl.clientWidth;
+            this.isOverflowing = isOverflowing;
+            if (isOverflowing) {
+                tabsEl.classList.add('dropdown-mode');
+                if (openMenu) tabsEl.classList.add('menu-open');
+            }
+        }
+    },
+    template: html`
+        <div class="tab-component">
+            <div class="tabs">
+                <button
+                    class="button-symbol white"
+                    @click="openHamburgerMenu"
+                    title="Show tab menu"
+                >≡</button>
+                <button
+                    v-for="tab in tabs"
+                    :key="tab.name"
+                    class="tab-button"
+                    :class="{ active: tab.name === internalActiveTab }"
+                    @click="selectTab(tab.name)"
+                    :data-tab="tab.name"
+                >
+                    {{ tab.label }}
+                    <span
+                        v-if="tab.closable"
+                        class="tab-close"
+                        @click.stop="closeTab(tab.name)"
+                        title="Close tab"
+                    >×</span>
+                </button>
+                <button
+                    v-if="showNewTabButton"
+                    class="new-tab-button"
+                    @click="addNewTab"
+                    title="New Tab"
+                >+</button>
+            </div>
+            <div class="tab-container">
+                <div
+                    v-for="tab in tabs"
+                    :key="tab.name + '-content'"
+                    v-show="tab.name === internalActiveTab"
+                    class="tab-content"
+                    :id="tab.name"
+                >
+                    <component
+                        v-if="tab.component"
+                        :is="tab.component"
+                        v-bind="tab.props || {}"
+                    ></component>
+                    <template v-else>
+                        <div v-html="tab.content"></div>
+                    </template>
+                </div>
+            </div>
+        </div>
+    `
+};
+
+export default TabComponent;
